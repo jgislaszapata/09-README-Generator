920ba00e8f9b84bb47402d982504bc95d1efe9f3
Jesus Islas <jesusislas96@gmail.com>
2022-07-09 23:16:32 -0400

    app working, added sample screnshot

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+const generate = require('./generateMarkdown');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
@@ -9,114 +10,68 @@ const questions = [
     {
         type: "input",
         message: "Enter your Github username",
-        name: "Username"
-    },
-    {
-        type: "input",
-        message: "Enter the name of your Github repository",
-        name: "reponame"
+        name: "username",
+        default: "jgislaszapta"
     },
     {
         type: "input",
         message: "Enter the title of your project",
-        name: "Project title"
+        name: "projectname",
+        default: "09-README-Generator"
     },
     {
         type: "input",
         message: "Give a description of your project",
-        name: "Description"
+        name: "description",
+        default: "This application is a README.md file generator that creates a professional README based on user input."
     },
     {
         type: "input",
         message: "Enter any installation steps required for your project, if applicable",
-        name: "Installation"
+        name: "installation",
+        default: "User will begin by cloning the repo and then installing the inquirer dependancy at version 6.5.0."
     },
     {
         type: "input",
         message: "Enter instructions and examples for the usage of your project",
-        name: "Usage"
+        name: "usage"
     },
     {
         type: "input",
         message: "Enter instructions detailing how other developers can contribute to your project",
-        name: "Contribution"
+        name: "contribution"
     },
     {
         type: "input",
         message: "Provide information on any tests written for this project and how they can be used",
-        name: "Testing"
+        name: "testing",
+        default: "No tests were written for this application."
     },
     {
         type: "list",
         message: "Choose a license for your project",
         choices: ["MIT license", "Mosilla Public license"],
-        name: "License"
+        name: "license"
     }
 
 ];
 
-inquirer.prompt(questions)
-.then((data) => {
-    const filename = `README.md`;
+// TODO: Create a function to write README file
+function writeToFile(filename, data) {
+    const content = generate.generateMarkdown(data);
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-  });
-
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//     fs.writeFile(fileName, data, err => {
-//         if (err) {
-//             console.log(err)
-//         } else {
-//             console.log("Success!")
-//         }
-//     })
-// }
+    fs.writeFile(filename, content, err => 
+      err ? console.log(err) : console.log("Success!"));
+    }
 
 // // TODO: Create a function to initialize app
-// function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        const filename = "README.md"
+        writeToFile(filename, data);
+    });
+}
 
 // // Function call to initialize app
-// init();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// inquirer.prompt([
-//     {
-//         type: 'input',
-//         message: 'Enter your Github username',
-//         name: 'userName'
-//     },
-//     {
-//         type: 'input',
-//         message: "Enter your Github repo's name",
-//         name: "repoName"
-//     },
-//     {
-//         type: 'input',
-//         message: "Enter a description of your repo",
-//         name: "repoDesc"
-//     }
-// ])
-// .then((data) => {
-//     console.log(data)
-//     console.log('...Program complete')
-// })
+init();
